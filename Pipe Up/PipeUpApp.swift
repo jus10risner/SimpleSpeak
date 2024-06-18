6491f636fff0162bdd1fc4e8b2bc28a92d475f82
Justin <justinrisner@gmail.com>
2024-06-18 10:46:41 -0400

    Initial Commit

---
+++ Pipe Up/PipeUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSpeakApp.swift
+//  QuickSpeak
+//
+//  Created by Justin Risner on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
