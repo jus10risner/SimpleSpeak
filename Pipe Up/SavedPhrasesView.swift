3ce10d2e3b1eccc3b79cc5e485e6147e3f847081
Justin <justinrisner@gmail.com>
2024-06-20 15:53:02 -0400

    [Feature] Set up list of saved phrases that can be deleted, and made each phrase a button that speaks it's text when tapped

--- Pipe Up/SavedPhrasesView.swift
+++ Pipe Up/SavedPhrasesView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct SavedPhrasesView: View {
+    @EnvironmentObject var vm: ViewModel
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(sortDescriptors: []) var savedPhrases: FetchedResults<Phrase>
+    
     var body: some View {
-        Text("Welcome to saved phrases")
+        NavigationStack {
+            List {
+                ForEach(savedPhrases) { phrase in
+                    Button {
+                        vm.speak(phrase.content)
+                    } label: {
+                        Text(phrase.content)
+                            .foregroundStyle(Color.primary)
+                    }
+                    .swipeActions(edge: .trailing) {
+                        Button {
+                            context.delete(phrase)
+                            try? context.save()
+                        } label: {
+                            Label("Delete Phrase", systemImage: "trash")
+                                .labelStyle(.iconOnly)
+                        }
+                        .tint(Color.red)
+                    }
+                }
+            }
+            .listRowSpacing(5)
+            .navigationTitle("Saved Phrases")
+        }
     }
 }
 
