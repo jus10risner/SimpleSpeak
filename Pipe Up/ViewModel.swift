023e77e431bc801c04cbbd38e80107599ba44f9b
Justin <justinrisner@gmail.com>
2024-06-27 09:56:53 -0400

    [Feature] Set AppStorage properties to publish changes; moved universal methods into view model; added delegate, to track when AVSpeechSynthesizer starts/finishes speaking

--- Pipe Up/ViewModel.swift
+++ Pipe Up/ViewModel.swift
@@ -8,14 +8,34 @@
 import AVFoundation
 import SwiftUI
 
-class ViewModel: ObservableObject {
-    @AppStorage("useDuringCalls") var useDuringCalls = true
-    @AppStorage("usePersonalVoice") var usePersonalVoice = false
-    @AppStorage("appAppearance") var appAppearance: AppearanceOptions = .automatic
-    
-    @Published var voiceToUse = AVSpeechSynthesisVoice(language: AVSpeechSynthesisVoice.currentLanguageCode())
-    
+class ViewModel: NSObject, ObservableObject {
     let synthesizer = AVSpeechSynthesizer()
+    @Published var voiceToUse = AVSpeechSynthesisVoice(language: Locale.preferredLanguages[0])
+    @Published var isSpeaking = false
+    
+    @AppStorage("useDuringCalls") var useDuringCalls = true {
+        willSet { objectWillChange.send() }
+    }
+    @AppStorage("usePersonalVoice") var usePersonalVoice = false {
+        willSet { objectWillChange.send() }
+    }
+    @AppStorage("appAppearance") var appAppearance: AppearanceOptions = .automatic {
+        willSet { objectWillChange.send() }
+    }
+    @AppStorage("selectedLanguage")  var selectedLanguage = Locale.preferredLanguages[0] {
+        willSet { objectWillChange.send() }
+    }
+    @AppStorage("selectedVoiceIdentifier")  var selectedPersonalVoiceIdentifier: String? {
+        willSet { objectWillChange.send() }
+    }
+    
+    override init() {
+        super.init()
+        self.synthesizer.delegate = self
+    }
+    
+    
+    // MARK: - Methods
     
     // Speak typed text aloud
     func speak(_ text: String) {
@@ -26,29 +46,78 @@ class ViewModel: ObservableObject {
         synthesizer.speak(utterance)
     }
     
-    // Set the voice to use for text-to-speech
-    func assignVoice() {
-        let languageCode = AVSpeechSynthesisVoice.currentLanguageCode()
-        
-        if #available(iOS 17, *) {
-            if AVSpeechSynthesizer.personalVoiceAuthorizationStatus == .authorized && self.usePersonalVoice == true {
+    // Request to use Personal Voice, if iOS 17 is available
+    @available(iOS 17, *)
+    func requestPersonalVoiceAuthorization() {
+        AVSpeechSynthesizer.requestPersonalVoiceAuthorization { status in
+            if status == .authorized {
                 print("I'm authorized to use Personal Voice!")
-                // Set in case none of the voices are Personal Voice
-                voiceToUse = AVSpeechSynthesisVoice(language: languageCode)
-                
-                for voice in AVSpeechSynthesisVoice.speechVoices() {
-                    if voice.voiceTraits == .isPersonalVoice {
-                        print("Personal Voice: \(voice.name)")
-                        voiceToUse = AVSpeechSynthesisVoice(identifier: voice.identifier)
-                    }
-                }
             } else {
                 print("Personal Voice not authorized")
-                voiceToUse = AVSpeechSynthesisVoice(language: languageCode)
+            }
+        }
+    }
+    
+    // Set the voice to use for text-to-speech
+    func assignVoice() {
+        if #available(iOS 17, *) {
+            let authorizationStatus = AVSpeechSynthesizer.personalVoiceAuthorizationStatus
+            let personalVoices = AVSpeechSynthesisVoice.speechVoices().filter { $0.voiceTraits == .isPersonalVoice }
+            
+            if self.usePersonalVoice == true && authorizationStatus == .authorized {
+                if personalVoices.count > 0 && self.selectedPersonalVoiceIdentifier == nil {
+                    self.selectedPersonalVoiceIdentifier = personalVoices.first?.identifier
+                }
+            } else {
+                self.selectedPersonalVoiceIdentifier = nil
+            }
+            
+            if let identifier = self.selectedPersonalVoiceIdentifier {
+                voiceToUse = AVSpeechSynthesisVoice(identifier: identifier)
+            } else {
+                voiceToUse = AVSpeechSynthesisVoice(language: selectedLanguage)
             }
         } else {
-            print("This device is not compatible with Personal Voice")
-            voiceToUse = AVSpeechSynthesisVoice(language: languageCode)
+            voiceToUse = AVSpeechSynthesisVoice(language: selectedLanguage)
         }
+        
+        
+//        if #available(iOS 17, *) {
+//            if AVSpeechSynthesizer.personalVoiceAuthorizationStatus == .authorized && self.usePersonalVoice == true {
+//                print("I'm authorized to use Personal Voice!")
+//                // Set in case none of the voices are Personal Voice
+//                voiceToUse = AVSpeechSynthesisVoice(language: languageCode)
+//                
+//                for voice in AVSpeechSynthesisVoice.speechVoices() {
+//                    if voice.voiceTraits == .isPersonalVoice {
+//                        print("Personal Voice: \(voice.name)")
+//                        voiceToUse = AVSpeechSynthesisVoice(identifier: voice.identifier)
+//                    }
+//                }
+//            } else {
+//                print("Personal Voice not authorized")
+//                voiceToUse = AVSpeechSynthesisVoice(identifier: self.selectedVoiceIdentifier)
+//                
+//            }
+//        } else {
+//            print("This device is not compatible with Personal Voice")
+//            voiceToUse = AVSpeechSynthesisVoice(identifier: self.selectedVoiceIdentifier)
+//        }
+    }
+}
+
+// Tracks when speech begins and ends, for displaying an indicator
+extension ViewModel: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        print("started")
+        self.isSpeaking = true
+    }
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) {}
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) {}
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {}
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {}
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        print("finished")
+        self.isSpeaking = false
     }
 }
