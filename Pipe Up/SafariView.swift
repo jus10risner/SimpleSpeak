7ddfb4810d6e3db69c39e54eecbe4c04f4fc06df
Justin <justinrisner@gmail.com>
2024-06-27 09:52:55 -0400

    [Feature] Added two different web browser views, to determine which is best for this app's use cases

--- Pipe Up/SafariView.swift
+++ Pipe Up/SafariView.swift
@@ -5,4 +5,18 @@
 //  Created by Justin Risner on 6/21/24.
 //
 
-import Foundation
+import SafariServices
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+
+    }
+
+}
--- Pipe Up/WebView.swift
+++ Pipe Up/WebView.swift
@@ -5,4 +5,20 @@
 //  Created by Justin Risner on 6/21/24.
 //
 
-import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: URL?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        if let url {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+}
