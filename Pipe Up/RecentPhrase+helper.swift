b1b67aa39b282fc2cfa3bb0cf25a98523f9366e6
Justin <justinrisner@gmail.com>
2024-06-27 09:51:59 -0400

    [Feature] Added a helper class, to bridge between the Core Data entity and views

--- Pipe Up/RecentPhrase+helper.swift
+++ Pipe Up/RecentPhrase+helper.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension RecentPhrase {
+    var text: String {
+        get { text_ ?? "" }
+        set { text_ = newValue }
+    }
+    
+    var timeStamp: Date {
+        get { timeStamp_ ?? Date() }
+        set { timeStamp_ = newValue }
+    }
+}
