b21ad50cc79c3a22725194325f32bfbbcb34ae8e
Justin <justinrisner@gmail.com>
2024-07-11 13:51:09 -0400

    [Feature] Extracted the list of saved phrases into its own view, for composability

---
+++ Pipe Up/Saved Phrases/SavedPhrasesListView.swift
@@ -0,0 +1,54 @@
+//
+//  SavedPhrasesListView.swift
+//  Pipe Up
+//
+//  Created by Justin Risner on 7/11/24.
+//
+
+import SwiftUI
+
+struct SavedPhrasesListView: View {
+    @Environment(\.managedObjectContext) var context
+    @EnvironmentObject var vm: ViewModel
+    
+    @FetchRequest(sortDescriptors: []) var savedPhrases: FetchedResults<SavedPhrase>
+    
+    let category: PhraseCategory?
+    
+    var body: some View {
+        List {
+            ForEach(filteredPhrases) { phrase in
+                Button {
+                    vm.speak(phrase.text)
+                } label: {
+                    Text(phrase.text)
+                        .foregroundStyle(Color.primary)
+                }
+                .swipeActions(edge: .trailing) {
+                    Button {
+                        withAnimation(.easeInOut) {
+                            context.delete(phrase)
+                            try? context.save()
+                        }
+                    } label: {
+                        Label("Delete Phrase", systemImage: "trash")
+                            .labelStyle(.iconOnly)
+                    }
+                    .tint(Color.red)
+                }
+            }
+        }
+        .navigationTitle(category?.title ?? "General")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    // Returns the phrases in the selected category
+    private var filteredPhrases: [SavedPhrase] {
+        return savedPhrases.filter({ $0.category == category })
+    }
+}
+
+#Preview {
+    SavedPhrasesListView(category: nil)
+        .environmentObject(ViewModel())
+}
