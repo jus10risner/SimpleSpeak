31014ffdac9c7c6b2cc823c3a2793eb16a22b1cf
Justin <justinrisner@gmail.com>
2024-07-11 09:17:49 -0400

    [Feature] Changed the view so that it displays categories, which a user can tap to see phrases within a given category

--- Pipe Up/SavedPhrasesView.swift
+++ Pipe Up/SavedPhrasesView.swift
@@ -11,13 +11,74 @@ struct SavedPhrasesView: View {
     @EnvironmentObject var vm: ViewModel
     @Environment(\.managedObjectContext) var context
     @FetchRequest(sortDescriptors: []) var savedPhrases: FetchedResults<SavedPhrase>
+    @FetchRequest(sortDescriptors: []) var categories: FetchedResults<PhraseCategory>
     
-    @State private var showingTextEntry = false
+    @State private var showingAddPhrase = false
     
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(savedPhrases) { phrase in
+//            allSavedPhrases
+            categoryList
+                .navigationTitle("Saved Phrases")
+                .listRowSpacing(5)
+                .scrollContentBackground(.hidden)
+                .background(Color(.systemGroupedBackground))
+                .scrollDismissesKeyboard(.interactively)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            showingAddPhrase = true
+                        } label: {
+                            Label("Add New Phrase", systemImage: "plus")
+                        }
+                    }
+                    
+                    ToolbarItemGroup(placement: .topBarLeading) {
+                        Button("Clear All") {
+                            for item in savedPhrases {
+                                context.delete(item)
+                                try? context.save()
+                            }
+                        }
+                        
+                        
+                    }
+                }
+                .sheet(isPresented: $showingAddPhrase) {
+                    AddSavedPhraseView()
+                }
+        }
+    }
+    
+    // List of categories, with navigation links to their respective phrases
+    private var categoryList: some View {
+        List {
+            ZStack {
+                Color.clear
+                NavigationLink("General") {
+                    uncategorizedPhrasesList
+                        .navigationTitle("General")
+                        .navigationBarTitleDisplayMode(.inline)
+                }
+            }
+            
+            ForEach(categories) { category in
+                NavigationLink {
+                    // TODO: Create category view, for listing phrases saved in a given category
+                    // Navigate to list of phrases for this category
+                    Text("Category: \(category.title)")
+                } label: {
+                    Text(category.title)
+                }
+            }
+        }
+    }
+    
+    // List of phrases with no category
+    private var uncategorizedPhrasesList: some View {
+        List {
+            ForEach(savedPhrases) { phrase in
+                if phrase.category == nil {
                     Button {
                         vm.speak(phrase.text)
                     } label: {
@@ -26,8 +87,10 @@ struct SavedPhrasesView: View {
                     }
                     .swipeActions(edge: .trailing) {
                         Button {
-                            context.delete(phrase)
-                            try? context.save()
+                            withAnimation(.easeInOut) {
+                                context.delete(phrase)
+                                try? context.save()
+                            }
                         } label: {
                             Label("Delete Phrase", systemImage: "trash")
                                 .labelStyle(.iconOnly)
@@ -36,23 +99,32 @@ struct SavedPhrasesView: View {
                     }
                 }
             }
-            .listRowSpacing(5)
-            .navigationTitle("Saved Phrases")
-            .scrollContentBackground(.hidden)
-            .background(Color(.systemGroupedBackground))
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
+        }
+    }
+    
+    // List of all saved phrases
+    private var allSavedPhrases: some View {
+        List {
+            ForEach(savedPhrases) { phrase in
+                Button {
+                    vm.speak(phrase.text)
+                } label: {
+                    Text(phrase.text)
+                        .foregroundStyle(Color.primary)
+                }
+                .swipeActions(edge: .trailing) {
                     Button {
-                        showingTextEntry = true
+                        withAnimation(.easeInOut) {
+                            context.delete(phrase)
+                            try? context.save()
+                        }
                     } label: {
-                        Label("Add New Phrase", systemImage: "plus")
+                        Label("Delete Phrase", systemImage: "trash")
+                            .labelStyle(.iconOnly)
                     }
+                    .tint(Color.red)
                 }
             }
-            .sheet(isPresented: $showingTextEntry, content: {
-                AddSavedPhraseView()
-                    .presentationDetents([.medium])
-            })
         }
     }
 }
