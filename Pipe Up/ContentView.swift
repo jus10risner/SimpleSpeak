30135bc5d6eb342e8d3c7bbb5d5ed3879c9ba4e3
Justin <justinrisner@gmail.com>
2024-07-11 11:45:50 -0400

    [Feature] Switched back to the default AccentColor property, when iOS 16 failed to tint tab bar symbols correctly

--- Pipe Up/ContentView.swift
+++ Pipe Up/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
                 .tag(3)
             
         }
-        .tint(Color(.defaultAccent))
+        .tint(Color("AccentColor"))
         .environmentObject(vm)
     }
 }
