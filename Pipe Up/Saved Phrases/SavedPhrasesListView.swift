f4e9cd399177da509a89343aa46f73d2db452576
Justin <justinrisner@gmail.com>
2024-07-19 10:24:40 -0400

    [Feature] Added the "Add Saved Phrase" button logic to this view, to allow adding a new saved phrase from directly within a category list

--- Pipe Up/Saved Phrases/SavedPhrasesListView.swift
+++ Pipe Up/Saved Phrases/SavedPhrasesListView.swift
@@ -11,9 +11,12 @@ struct SavedPhrasesListView: View {
     @Environment(\.managedObjectContext) var context
     @EnvironmentObject var vm: ViewModel
     
+    @FetchRequest(sortDescriptors: []) var categories: FetchedResults<PhraseCategory>
     @FetchRequest var savedPhrases: FetchedResults<SavedPhrase>
     
     let category: PhraseCategory?
+    
+    @State private var showingAddPhrase = false
     
     // Custom init, so I can pass in the optional "category" property as a predicate
     init(category: PhraseCategory?) {
@@ -37,18 +40,18 @@ struct SavedPhrasesListView: View {
                     vm.speak(phrase.text)
                 } label: {
                     if let label = phrase.label {
-                        Text("\(label)(\(phrase.displayOrder))")
+//                        Text("\(label)(\(phrase.displayOrder))")
+                        Text(label)
                     } else {
-                        Text("\(phrase.text)(\(phrase.displayOrder))")
+//                        Text("\(phrase.text)(\(phrase.displayOrder))")
+                        Text(phrase.text)
                     }
                 }
                 .foregroundStyle(Color.primary)
                 .swipeActions(edge: .trailing) {
-                    Button {
-                        withAnimation(.easeInOut) {
-                            context.delete(phrase)
-                            try? context.save()
-                        }
+                    Button(role: .destructive) {
+                        context.delete(phrase)
+                        try? context.save()
                     } label: {
                         Label("Delete Phrase", systemImage: "trash")
                             .labelStyle(.iconOnly)
@@ -59,9 +62,9 @@ struct SavedPhrasesListView: View {
             .onMove(perform: { indices, newOffset in
                 move(from: indices, to: newOffset)
             })
-            .onDelete(perform: { indexSet in
-                vm.deletePhrase(at: indexSet, from: savedPhrases)
-            })
+//            .onDelete(perform: { indexSet in
+//                vm.deletePhrase(at: indexSet, from: savedPhrases)
+//            })
             
             // TODO: Remove this, when finished testing
             #if DEBUG
@@ -74,6 +77,20 @@ struct SavedPhrasesListView: View {
             #endif
         }
         .listRowSpacing(vm.listRowSpacing)
+        .toolbar {
+            if categories.count != 0 {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showingAddPhrase = true
+                    } label: {
+                        Label("Add New Phrase", systemImage: "plus")
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddPhrase) {
+            AddSavedPhraseView(category: category)
+        }
     }
     
     // Persists the order of vehicles, after moving
