a80a2529e2f912982701a7514f3a68f1da422248
Justin <justinrisner@gmail.com>
2024-08-09 11:08:14 -0400

    [Feature] Added a selector for recently-used phrases, instead of using a separate sheet to display them

--- Pipe Up/Communication/CategorySelectorView.swift
+++ Pipe Up/Communication/CategorySelectorView.swift
@@ -12,16 +12,21 @@ struct CategorySelectorView: View {
     @FetchRequest(sortDescriptors: []) var categories: FetchedResults<PhraseCategory>
     
     @Binding var selectedCategory: PhraseCategory?
+    @Binding var showingRecents: Bool
     
     let rows = [ GridItem(.adaptive(minimum: 150), spacing: 5) ]
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHGrid(rows: rows, spacing: 5) {
+                recentPhrasesButton()
+                
                 categoryButton(category: nil, text: "General")
                 
                 ForEach(categories, id: \.id) { category in
-                    categoryButton(category: category, text: category.title)
+                    if category.phrases?.count != 0 {
+                        categoryButton(category: category, text: category.title)
+                    }
                 }
             }
             .padding(.horizontal)
@@ -30,9 +35,28 @@ struct CategorySelectorView: View {
 //        .padding(.bottom, 5)
     }
     
+    private func recentPhrasesButton() -> some View {
+        Button {
+            showingRecents = true
+            selectedCategory = nil
+        } label: {
+            Text("Recents")
+                .foregroundStyle(Color.primary)
+                .padding(15)
+                .background(Color(.secondarySystemGroupedBackground), in: RoundedRectangle(cornerRadius: 15))
+        }
+        .overlay {
+            if selectedCategory == nil && showingRecents == true {
+                RoundedRectangle(cornerRadius: vm.cornerRadius)
+                    .stroke(Color.secondary, lineWidth: 0.5)
+            }
+        }
+    }
+    
     // Category selection button
     private func categoryButton(category: PhraseCategory?, text: String) -> some View {
         Button {
+            showingRecents = false
             selectedCategory = category
         } label: {
             Text(text)
@@ -41,7 +65,7 @@ struct CategorySelectorView: View {
                 .background(Color(.secondarySystemGroupedBackground), in: RoundedRectangle(cornerRadius: 15))
         }
         .overlay {
-            if selectedCategory == category {
+            if selectedCategory == category && showingRecents == false {
                 RoundedRectangle(cornerRadius: vm.cornerRadius)
                     .stroke(Color.secondary, lineWidth: 0.5)
             }
@@ -50,6 +74,6 @@ struct CategorySelectorView: View {
 }
 
 #Preview {
-    CategorySelectorView(selectedCategory: .constant(nil))
+    CategorySelectorView(selectedCategory: .constant(nil), showingRecents: .constant(false))
         .environmentObject(ViewModel())
 }
