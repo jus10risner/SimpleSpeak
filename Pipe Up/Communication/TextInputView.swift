19bc3a9fd48b151360b4968995d572dcc0a2337c
Justin <justinrisner@gmail.com>
2024-09-05 15:42:55 -0400

    [Feature] Totally redesigned TextInputView, to make the text field an overlay with a material background, rather than keeping it inline with the phrase cards

--- Pipe Up/Communication/TextInputView.swift
+++ Pipe Up/Communication/TextInputView.swift
@@ -9,151 +9,220 @@ import SwiftUI
 
 struct TextInputView: View {
     @Environment(\.managedObjectContext) var context
+    @Environment(\.dismiss) var dismiss
     @EnvironmentObject var vm: ViewModel
     
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \SavedPhrase.displayOrder, ascending: true)]) var allPhrases: FetchedResults<SavedPhrase>
     
     @State private var text = ""
     @State private var mostRecentTypedPhrase = ""
-    @State private var showingTextFieldButtons = false
+    @Binding var showingTextField: Bool
     
     @FocusState var isInputActive: Bool
     
     var body: some View {
-        VStack(spacing: 0) {
-            if showingTextFieldButtons {
-                textFieldButtons
-            }
+        ZStack(alignment: .bottom) {
+            Color.clear
+                .ignoresSafeArea()
+                .background(.ultraThinMaterial)
             
-            textField
-        }
-        .background(Color(.secondarySystemGroupedBackground), in: RoundedRectangle(cornerRadius: vm.cornerRadius))
-        .padding()
-        .onChange(of: isInputActive) { bool in
-            withAnimation(.snappy) {
-                showingTextFieldButtons = bool
+            VStack(spacing: 0) {
+                textFieldButtons
+                
+                textField
             }
+            .background(Color(.secondarySystemGroupedBackground), in: RoundedRectangle(cornerRadius: vm.cornerRadius))
+            .padding()
+        }
+        .onAppear {
+            isInputActive = true
         }
     }
     
     // MARK: - Views
     
     private var textField: some View {
-        TextField("Tap to type...", text: $text, axis: .vertical)
-            .padding()
-            .lineLimit(5)
+        TextField("Type to speak...", text: $text, axis: .vertical)
+            .padding([.horizontal, .bottom])
+            .padding(.top, 10)
             .font(.title3)
             .focused($isInputActive)
             .submitLabel(.send)
             .onChange(of: text) { newValue in
                 // This serves as a replacement for onSubmit, when a vertical axis is used on TextField
-                guard let newValueLastChar = newValue.last else { return }
-                if newValueLastChar == "\n" {
+                guard let newValueLastCharacter = newValue.last else { return }
+                if newValueLastCharacter == "\n" {
                     text.removeLast()
                     submitAndAddRecent()
-                    
                 }
             }
             .onSubmit {
                 // This serves to keep TextField focused if a hardware keyboard is used
-                withAnimation(.easeInOut) {
-                    submitAndAddRecent()
-                }
+                submitAndAddRecent()
             }
     }
     
     private var textFieldButtons: some View {
         HStack {
-            repeatPhraseButton
-                .opacity(mostRecentTypedPhrase == "" ? 0 : 1)
+            if mostRecentTypedPhrase != "" && vm.synthesizerState == .inactive {
+                TextInputButton(text: "Repeat Last Typed Phrase", symbolName: "repeat.circle.fill", action: {
+                    vm.speak(mostRecentTypedPhrase)
+                })
+                .foregroundStyle(Color.secondary)
+                .transition(.opacity.animation(.default))
+            }
             
             Spacer()
             
-//            if vm.synthesizerState == .speaking {
-//                pauseSpeakingButton
-//            } else if vm.synthesizerState == .paused {
-//                cancelSpeakingButton
-//
-//                continueSpeakingButton
-//            }
+            speechControlButtons
+                .transition(.opacity.animation(.default))
             
-//            clearTextButton
-//                .opacity(text == "" ? 0 : 1)
-//                .animation(.easeInOut, value: text)
+            if text != "" {
+                TextInputButton(text: "Clear Text", symbolName: "trash.circle.fill") {
+                    withAnimation {
+                        text = ""
+                    }
+                }
+                .foregroundStyle(Color.secondary)
+                .transition(.opacity.animation(.default))
+            }
             
-            dismissTextFieldButton
+            TextInputButton(text: "Dismiss Keyboard", symbolName: "xmark.circle.fill") {
+                dismissKeyboard()
+            }
+            .foregroundStyle(Color.secondary)
         }
-        .padding(10)
+        .padding([.top, .horizontal])
+        .padding(.bottom, 0)
+        .drawingGroup()
+    }
+    
+    private var speechControlButtons: some View {
+        Group {
+            if vm.synthesizerState == .speaking {
+                TextInputButton(text: "Pause Speech", symbolName: "pause.circle.fill") {
+                    vm.pauseSpeaking()
+                }
+            } else if vm.synthesizerState == .paused {
+                TextInputButton(text: "Cancel Speech", symbolName: "stop.circle.fill") {
+                    vm.cancelSpeaking()
+                }
+                
+                TextInputButton(text: "Continue Speech", symbolName: "play.circle.fill") {
+                    vm.continueSpeaking()
+                }
+            }
+        }
     }
     
     
     // MARK: - Buttons
+    /**
+//    private var dismissTextFieldButton: some View {
+//        Button {
+//            mostRecentTypedPhrase = ""
+//            vm.cancelSpeaking()
+//            isInputActive = false
+//
+//            withAnimation {
+//                text = ""
+//
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+//                    showingTextField = false
+//                }
+//            }
+//        } label: {
+//            Label("Dismiss Keyboard", systemImage: "xmark.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//        .foregroundStyle(Color.secondary)
+//    }
     
-    private var dismissTextFieldButton: some View {
-        Button {
-            withAnimation {
-                text = ""
-                mostRecentTypedPhrase = ""
-                vm.cancelSpeaking()
-                isInputActive = false
-            }
-        } label: {
-            Label("Dismiss Keyboard", systemImage: "xmark.circle.fill")
-                .labelStyle(.iconOnly)
-                .font(.title)
-                .foregroundStyle(Color.secondary)
-                .symbolRenderingMode(.hierarchical)
-        }
-        .buttonStyle(.plain)
-    }
+//    private var clearTextButton: some View {
+//        Button {
+//            withAnimation {
+//                text = ""
+//            }
+//        } label: {
+//            Label("Clear Text", systemImage: "trash.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//        .foregroundStyle(Color.secondary)
+//    }
     
-    private var clearTextButton: some View {
-        Button {
-            text = ""
-        } label: {
-            Label("Clear Text", systemImage: "trash.circle.fill")
-                .labelStyle(.iconOnly)
-                .font(.title)
-                .foregroundStyle(Color.secondary)
-                .symbolRenderingMode(.hierarchical)
-        }
-        .buttonStyle(.plain)
-    }
+//    private var repeatPhraseButton: some View {
+//        Button {
+//            vm.speak(mostRecentTypedPhrase)
+//        } label: {
+//            Label("Repeat Last Typed Phrase", systemImage: "repeat.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//        .foregroundStyle(Color.secondary)
+//    }
     
-    private var repeatPhraseButton: some View {
-        Button {
-            vm.speak(mostRecentTypedPhrase)
-        } label: {
-            Label("Speak Last Typed Phrase", systemImage: "repeat.circle.fill")
-                .labelStyle(.iconOnly)
-                .font(.title)
-                .foregroundStyle(Color.secondary)
-                .symbolRenderingMode(.hierarchical)
-        }
-        .buttonStyle(.plain)
-    }
+//    private var cancelSpeakingButton: some View {
+//        Button {
+//            vm.cancelSpeaking()
+//        } label: {
+//            Label("Cancel Speech", systemImage: "stop.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//    }
+    
+//    private var pauseSpeakingButton: some View {
+//        Button {
+//            vm.pauseSpeaking()
+//        } label: {
+//            Label("Pause Speech", systemImage: "pause.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//    }
+    
+//    private var continueSpeakingButton: some View {
+//        Button {
+//            vm.continueSpeaking()
+//        } label: {
+//            Label("Continue Speech", systemImage: "play.circle.fill")
+//        }
+//        .textInputButtonStyle()
+//    }
+     */
     
     
     // MARK: - Methods
+    
+    func dismissKeyboard() {
+        mostRecentTypedPhrase = ""
+        vm.cancelSpeaking()
+        isInputActive = false
+        
+        withAnimation {
+            text = ""
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                showingTextField = false
+            }
+        }
+    }
     
     func submitAndAddRecent() {
         isInputActive = true
         
         if text != "" {
+            vm.speak(text)
+            
+            // If phrase doesn't already exist in Recents, add it
+            if !allPhrases.contains(where: { $0.category == nil && $0.text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) == text.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) }) {
+                let newSavedPhrase = SavedPhrase(context: context)
+                newSavedPhrase.id = UUID()
+                newSavedPhrase.text = text.trimmingCharacters(in: .whitespacesAndNewlines)
+                newSavedPhrase.displayOrder = (allPhrases.last?.displayOrder ?? 0) + 1
+                try? context.save()
+            }
+            
             mostRecentTypedPhrase = text
             
-            withAnimation(.easeInOut) {
-                vm.speak(text)
-                
-                // If phrase doesn't already exist in Recents, add it
-                if !allPhrases.contains(where: { $0.category == nil && $0.text == text }) {
-                    let newSavedPhrase = SavedPhrase(context: context)
-                    newSavedPhrase.id = UUID()
-                    newSavedPhrase.text = text
-                    newSavedPhrase.displayOrder = (allPhrases.last?.displayOrder ?? 0) + 1
-                    try? context.save()
-                }
-                
+            withAnimation {
                 text = ""
             }
         }
@@ -161,6 +230,6 @@ struct TextInputView: View {
 }
 
 #Preview {
-    TextInputView()
+    TextInputView(showingTextField: .constant(false))
         .environmentObject(ViewModel())
 }
