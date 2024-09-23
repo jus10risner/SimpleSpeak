d7d011bf1a54dbf01c2f3b56aefcf0d1a640e64d
Justin <justinrisner@gmail.com>
2024-09-23 11:06:40 -0400

    [Feature] Added haptic tap to hovering button, for

--- Pipe Up/Communication/Buttons/HoveringButton.swift
+++ Pipe Up/Communication/Buttons/HoveringButton.swift
@@ -5,27 +5,58 @@
 //  Created by Justin Risner on 9/5/24.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct HoveringButton: View {
+    @State private var engine: CHHapticEngine?
+    
     var text: String
     var symbolName: String
     var action: () -> ()
     
     var body: some View {
-//        Circle()
-//            .frame(width: 50, height: 50)
-//            .foregroundStyle(Color(.defaultAccent))
-//            .shadow(radius: 5)
-//            .overlay {
-                Button(action: action, label: {
-                    Label(text, systemImage: symbolName)
-                        .labelStyle(.iconOnly)
-                        .font(.title3)
-                        .foregroundStyle(Color.white)
-                        .padding()
-                })
-//            }
+        Button {
+            buttonTappedHaptic()
+            action()
+        } label: {
+            Label(text, systemImage: symbolName)
+                .labelStyle(.iconOnly)
+                .font(.title3)
+                .foregroundStyle(Color.white)
+                .padding()
+        }
+        .onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("Error creating engine. Error: \(error.localizedDescription)")
+        }
+    }
+    
+    func buttonTappedHaptic() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 0.5)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
+        
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern. Error: \(error.localizedDescription)")
+        }
     }
 }
 
