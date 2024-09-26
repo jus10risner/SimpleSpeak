f5bbfbb065d13ae5736d5f941ff41f61b53f0bc7
Justin <justinrisner@gmail.com>
2024-09-26 09:08:20 -0400

    [Clean] Removed unused code

--- Pipe Up/ContentView.swift
+++ Pipe Up/ContentView.swift
@@ -8,35 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @StateObject var vm = ViewModel()
-//    @State private var selectedTab = 1
     
     var body: some View {
         CommunicationView()
-//        TabView(selection: $selectedTab)  {
-//            CommunicationView()
-//                .tabItem { Label("Speak", systemImage: "person.wave.2") }
-//                .tag(1)
-//            
-//            CategoriesListView()
-//                .tabItem { Label("Phrases", systemImage: "bookmark") }
-//                .tag(2)
-//            
-//            SettingsView()
-//                .tabItem { Label("Settings", systemImage: "gearshape") }
-//                .tag(3)
-//            
-//        }
-//        .tint(Color("AccentColor"))
-//        .environmentObject(vm)
     }
 }
 
 #Preview {
-    let controller = DataController(inMemory: true)
-    let context = controller.container.viewContext
-    
-    return ContentView()
-        .environment(\.managedObjectContext, context)
-        .environmentObject(ViewModel())
+    ContentView()
 }
