de1d33b403489ba829e504e1a462e641dac09ea4
Justin <justinrisner@gmail.com>
2024-10-14 10:26:17 -0400

    [Feature] Added WebView, to show simple instructional web pages (didn't need navigation and sharing features, as seen in SafariView); deleted EmptyListView and moved the code inline in the view where it will be shown

---
+++ Pipe Up/Settings/WebView.swift
@@ -0,0 +1,8 @@
+//
+//  WebView.swift
+//  Pipe Up
+//
+//  Created by Justin Risner on 10/10/24.
+//
+
+import Foundation
---
+++ Pipe Up/TestViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestViewModel.swift
+//  Pipe Up
+//
+//  Created by Justin Risner on 9/26/24.
+//
+
+import Foundation
