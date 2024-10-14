fdea95d8c3b47e8dedce5acb6d9b9cb591065137
Justin <justinrisner@gmail.com>
2024-10-14 11:05:01 -0400

    [Feature] Added sheets that display info for Personal Voice and speech voice selection, using a view that utilizes WebView

--- Pipe Up/Settings/SettingsView.swift
+++ Pipe Up/Settings/SettingsView.swift
@@ -12,6 +12,9 @@ struct SettingsView: View {
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var vm: ViewModel
     
+    @State private var showingPersonalVoiceSetupSheet = false
+    @State private var showingVoiceSelectionSheet = false
+    
     var body: some View {
         NavigationStack {
             List {
@@ -20,15 +23,18 @@ struct SettingsView: View {
                         Label("Use During Calls", systemImage: "phone.fill")
                     })
                 } footer: {
-//                    Text("Determines whether your typed phrases are spoken aloud to the other party on a phone call.")
-                    Text("Allows this app to send your spoken audio to other parties on a phone call or FaceTime.")
+                    Text("Sends speech to other parties on a phone call or FaceTime.")
                 }
                 
-//                if #available(iOS 17, *) {
-//                    if AVSpeechSynthesizer.personalVoiceAuthorizationStatus != .unsupported {
-                        PersonalVoiceSettingsView()
+                if #available(iOS 17, *) {
+//                    if AVSpeechSynthesizer.personalVoiceAuthorizationStatus == .authorized {
+                    PersonalVoiceSettingsView(showingpersonalVoiceSetupSheet: $showingPersonalVoiceSetupSheet)
 //                    }
-//                }
+                }
+                
+                Button("Change your speech voice") {
+                    showingVoiceSelectionSheet = true
+                }
                 
                 // TODO: Re-enable conditional
 //                if Locale.preferredLanguages.count > 1 {
@@ -57,6 +63,12 @@ struct SettingsView: View {
             .tint(Color(.defaultAccent))
 //            .navigationTitle("Settings")
             .navigationBarTitleDisplayMode(.inline)
+            .sheet(isPresented: $showingPersonalVoiceSetupSheet, content: {
+                webView(url: URL(string: "https://support.apple.com/en-us/104993")!, title: "Personal Voice", selection: $showingPersonalVoiceSetupSheet)
+            })
+            .sheet(isPresented: $showingVoiceSelectionSheet, content: {
+                webView(url: URL(string: "https://support.apple.com/en-us/111798")!, title: "Voice Selection", selection: $showingVoiceSelectionSheet)
+            })
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
                     Text("Settings")
@@ -76,6 +88,22 @@ struct SettingsView: View {
                     .buttonStyle(.plain)
                 }
             }
+        }
+    }
+    
+    private func webView(url: URL, title: String, selection: Binding<Bool>) -> some View {
+        NavigationStack {
+            WebView(url: url)
+                .ignoresSafeArea(edges: .bottom)
+                .navigationTitle(title)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Done") {
+                            selection.wrappedValue = false
+                        }
+                    }
+                }
         }
     }
 }
