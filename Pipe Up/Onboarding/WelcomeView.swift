612384a6288667c79798847ceb227220bb4a00f5
Justin <justinrisner@gmail.com>
2024-10-21 12:05:23 -0400

    [Feature] Broke the WelcomeView informational items out into a separate view, for better composability, and updated WelcomeView to incorporate them

--- Pipe Up/Onboarding/WelcomeView.swift
+++ Pipe Up/Onboarding/WelcomeView.swift
@@ -9,68 +9,52 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack {
-            Spacer()
+        VStack(alignment: .center) {
+           Spacer()
             
-            Text("Welcome to Pipe Up")
-                .font(.largeTitle.bold())
-            
-//            Spacer()
-            
-            speakInfo
-            
-            savePhrase
-            
-            Spacer()
+            VStack {
+//                Image("Primary Icon")
+//                    .clipShape(RoundedRectangle(cornerRadius: 12))
+//                    .padding(.bottom, 10)
+                
+                Text("Welcome to")
+                
+                Text("The App")
+            }
+            .font(.largeTitle.bold())
 
+            VStack(alignment: .leading) {
+                InformationItemView(title: "Speak", subtitle: "Type phrases to have the app speak them out loud.", imageName: "person.wave.2.fill")
+                
+                InformationItemView(title: "Save", subtitle: "Add your own commonly-used phrases, and speak them with a tap.", imageName: "bookmark.fill")
+                
+                InformationItemView(title: "Call", subtitle: "Use the app to speak phrases over the phone and in FaceTime calls.", imageName: "phone.fill")
+            }
+            
+            Spacer()
+            
+            Button {
+                // Dismiss button
+            } label: {
+                buttonLabel
+            }
+            .buttonStyle(.plain)
         }
+        .interactiveDismissDisabled()
         .padding(.horizontal)
     }
     
-    private var speakInfo: some View {
-        HStack(alignment: .center) {
-            Image(systemName: "keyboard")
-                .font(.largeTitle)
-                .foregroundColor(Color(.defaultAccent))
-                .padding()
-                .accessibility(hidden: true)
-
-            VStack(alignment: .leading) {
-                Text("Speak")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                    .accessibility(addTraits: .isHeader)
-
-                Text("Type phrases to have the app speak them out loud.")
-                    .font(.body)
-                    .foregroundColor(.secondary)
-                    .fixedSize(horizontal: false, vertical: true)
-            }
+    private var buttonLabel: some View {
+        Text("Continue")
+        .foregroundColor(.white)
+        .font(.headline)
+        .padding()
+        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+        .background {
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color(.defaultAccent))
         }
-        .padding(.top)
-    }
-    
-    private var savePhrase: some View {
-        HStack(alignment: .center) {
-            Image(systemName: "bookmark")
-                .font(.largeTitle)
-                .foregroundColor(Color(.defaultAccent))
-                .padding()
-                .accessibility(hidden: true)
-
-            VStack(alignment: .leading) {
-                Text("Save Phrases")
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                    .accessibility(addTraits: .isHeader)
-
-                Text("Add your own commonly-used phrases, and speak them with a tap.")
-                    .font(.body)
-                    .foregroundColor(.secondary)
-                    .fixedSize(horizontal: false, vertical: true)
-            }
-        }
-        .padding(.top)
+        .padding(.bottom)
     }
 }
 
