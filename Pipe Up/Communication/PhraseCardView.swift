fd3189726e031624654706649b5bd1bf9ba14ea0
Justin <justinrisner@gmail.com>
2024-11-13 11:10:23 -0500

    [Feature] Updated to remove logic related to recent phrases, after creating a separate RecentsCardView

--- Pipe Up/Communication/PhraseCardView.swift
+++ Pipe Up/Communication/PhraseCardView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 
 struct PhraseCardView: View {
     @EnvironmentObject var vm: ViewModel
-    @FetchRequest(sortDescriptors: [], animation: .easeInOut) var savedPhrases: FetchedResults<SavedPhrase>
+    @FetchRequest var savedPhrases: FetchedResults<SavedPhrase>
     
-    let category: PhraseCategory?
+    let category: PhraseCategory
     @Binding var showingAddPhrase: Bool
+    
+    // Custom init, so I can pass in the category property as a predicate
+    init(category: PhraseCategory, showingAddPhrase: Binding<Bool>) {
+        self.category = category
+        self._showingAddPhrase = showingAddPhrase
+        let predicate = NSPredicate(format: "category == %@", category)
+        
+        self._savedPhrases = FetchRequest(entity: SavedPhrase.entity(), sortDescriptors: [
+            NSSortDescriptor(
+                keyPath: \SavedPhrase.displayOrder,
+                ascending: true)
+        ], predicate: predicate, animation: .easeInOut)
+    }
     
     let columns = [GridItem(.adaptive(minimum: 150), spacing: 5)]
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 5) {
-                ForEach(filteredPhrases) { phrase in
+                ForEach(savedPhrases) { phrase in
                     Button {
                         if vm.synthesizerState != .inactive {
                             vm.cancelSpeaking()
@@ -27,72 +40,30 @@ struct PhraseCardView: View {
                         
                         vm.speak(phrase.text)
                     } label: {
-                        Group {
-                            if phrase.label != "" {
-                                Text(phrase.label)
-                            } else {
-                                Text(phrase.text)
+                        ZStack {
+                            Group {
+                                if phrase.label != "" {
+                                    Text(phrase.label)
+                                } else {
+                                    Text(phrase.text)
+                                }
                             }
+                            .font(.headline)
+                            .minimumScaleFactor(0.9)
+                            .frame(maxWidth: .infinity)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .frame(height: 100)
                         }
-                        .font(.headline)
-                        .minimumScaleFactor(0.9)
-                        .frame(maxWidth: .infinity)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                        .frame(height: 100)
                         .background(Color(.secondarySystemGroupedBackground), in: RoundedRectangle(cornerRadius: vm.cornerRadius))
                     }
                     .buttonStyle(.plain)
                 }
                 
-                if category != nil {
-                    addPhraseButton
-                }
-                
-//                if category == nil && filteredPhrases.count == 0 {
-//                    Text("No Recents")
-//                        .font(.headline)
-//                        .foregroundStyle(Color.secondary)
-//                        .frame(maxWidth: .infinity)
-//                        .frame(height: 100)
-//                        .overlay {
-//                            RoundedRectangle(cornerRadius: vm.cornerRadius)
-//                                .strokeBorder(style: StrokeStyle(lineWidth: 2, dash: [7]))
-//                                .foregroundStyle(Color.secondary)
-//                                .opacity(0.5)
-//                        }
-//                } else {
-//                    addPhraseButton
-//                }
+                addPhraseButton
             }
             .padding([.horizontal, .bottom])
         }
-//        .overlay {
-//            if category == nil && filteredPhrases.count == 0 {
-//                VStack(spacing: 15) {
-////                    Spacer()
-////                    Spacer()
-//                    
-//                    Image(systemName: "clock.arrow.circlepath")
-//                        .font(.system(size: 60))
-//                        .foregroundStyle(Color.secondary)
-//                        .opacity(0.5)
-//                    
-//                    Text("No Recent Phrases")
-//                        .font(.title3.bold())
-////                    Text("Tap to type a phrase")
-////
-////                    Image(systemName: "arrow.down")
-////                        .font(.largeTitle)
-////                        .foregroundStyle(Color.secondary)
-////
-////                    Spacer()
-////                    Spacer()
-////                    Spacer()
-//                }
-////                .padding(.bottom, 100)
-//            }
-//        }
     }
     
     private var addPhraseButton: some View {
@@ -112,18 +83,13 @@ struct PhraseCardView: View {
                 }
         }
     }
-    
-    // Returns the phrases in the selected category
-    private var filteredPhrases: [SavedPhrase] {
-        if let category {
-            return savedPhrases.filter({ $0.category == category }).sorted(by: { $0.displayOrder < $1.displayOrder })
-        } else {
-            return savedPhrases.filter({ $0.category == category }).sorted(by: { $0.displayOrder > $1.displayOrder })
-        }
-    }
 }
 
 #Preview {
-    PhraseCardView(category: nil, showingAddPhrase: .constant(false))
+    let context = DataController.preview.container.viewContext
+    let category = PhraseCategory(context: context)
+    category.title = "Favorites"
+    
+    return PhraseCardView(category: category, showingAddPhrase: .constant(false))
         .environmentObject(ViewModel())
 }
