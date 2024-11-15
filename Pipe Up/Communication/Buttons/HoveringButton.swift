851caa77ed17a8399f0632e8a2f8754bbcbb787f
Justin <justinrisner@gmail.com>
2024-11-15 10:36:53 -0500

    [Feature] Increased the intensity of the haptic feedback on the hovering button, to make it feel more decisive

--- Pipe Up/Communication/Buttons/HoveringButton.swift
+++ Pipe Up/Communication/Buttons/HoveringButton.swift
@@ -44,7 +44,7 @@ struct HoveringButton: View {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
         
         var events = [CHHapticEvent]()
-        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 0.5)
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1.0)
         let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
         
         let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
