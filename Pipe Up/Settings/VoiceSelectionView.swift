d4265b0c0b5573b62d0cd92dae5c9838aadd06fb
Justin <justinrisner@gmail.com>
2024-12-20 12:11:38 -0500

    [Feature] Updated documentation; modified copy

--- Pipe Up/Settings/VoiceSelectionView.swift
+++ Pipe Up/Settings/VoiceSelectionView.swift
@@ -18,9 +18,9 @@ struct VoiceSelectionView: View {
     
     var body: some View {
         List {
-            if !personalVoices.isEmpty {
+            if !personalVoices.isEmpty { // Only show this section if Personal Voices exist
                 Section {
-                    Picker("Personal Voices", selection: $vm.selectedVoiceIdentifier) {
+                    Picker("Personal Voice", selection: $vm.selectedVoiceIdentifier) {
                         ForEach(personalVoices, id: \.identifier) { voice in
                             Button(voice.name) {
                                 vm.selectedVoiceIdentifier = voice.identifier
@@ -32,14 +32,14 @@ struct VoiceSelectionView: View {
                     .labelsHidden()
                     .pickerStyle(.inline)
                 } header: {
-                    Text("Personal Voices")
+                    Text("Personal Voice")
                 } footer: {
-                    Text("To manage, go to Settings > Accessiblity > Personal Voice.")
+                    Text("Manage in Settings > Accessiblity > Personal Voice")
                 }
             }
             
             Section {
-                Picker("Standard", selection: $vm.selectedVoiceIdentifier) {
+                Picker("System", selection: $vm.selectedVoiceIdentifier) {
                     ForEach(standardVoices, id: \.identifier) { voice in
                         Button(voice.name) {
                             vm.selectedVoiceIdentifier = voice.identifier
@@ -51,9 +51,9 @@ struct VoiceSelectionView: View {
                 .labelsHidden()
                 .pickerStyle(.inline)
             } header: {
-                Text("Standard")
+                Text("System")
             } footer: {
-                Text("To manage voices, go to Settings > Accessiblity > Spoken Content > Voices")
+                Text("Manage speech voices in Settings > Accessiblity > Spoken Content > Voices")
             }
             
             if !noveltyVoices.isEmpty {
@@ -73,7 +73,7 @@ struct VoiceSelectionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .task { await loadVoices() }
         .onChange(of: scenePhase) { newValue in
-            // Updates the list of voices, when a user downloads or deletes a speech synthesis voice in the Settings app
+            // Updates the list of voices, in case a user adds/deletes/modifies a speech synthesis voice in the Settings app
             if newValue == .active {
                 Task { await loadVoices() }
             }
@@ -97,6 +97,7 @@ struct VoiceSelectionView: View {
     }
 }
 
-#Preview {
-    VoiceSelectionView()
-}
+//#Preview {
+//    VoiceSelectionView()
+//        .environmentObject(ViewModel())
+//}
