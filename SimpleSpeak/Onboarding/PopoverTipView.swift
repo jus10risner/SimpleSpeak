c51ea8a8ee08f3ea54d185264dfd8fc89c5cf791
Justin <justinrisner@gmail.com>
2025-02-25 15:47:04 -0500

    [Feature] Updated the view to accept a title, in addition to the text property; modified the background to be of the Material type

--- SimpleSpeak/Onboarding/PopoverTipView.swift
+++ SimpleSpeak/Onboarding/PopoverTipView.swift
@@ -9,23 +9,34 @@ import SwiftUI
 
 struct PopoverTipView: View {
     let symbolName: String
+    let title: String?
     let text: String
     
     var body: some View {
         HStack {
             Image(systemName: symbolName)
                 .font(.title)
-                .foregroundStyle(Color.secondary)
+                .foregroundStyle(Color(.defaultAccent))
                 .padding(.trailing)
             
-            Text(text)
+            VStack(alignment: .leading, spacing: 5) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                
+                Text(text)
+                    .font(.subheadline)
+                    .foregroundStyle(Color.secondary)
+            }
         }
         .padding()
         .frame(width: 300)
         .presentationCompactAdaptation(.popover)
+        .presentationBackground(.regularMaterial)
     }
 }
 
 #Preview {
-    PopoverTipView(symbolName: "hand.tap.fill", text: "Tap me!")
+    PopoverTipView(symbolName: "hand.tap.fill", title: "Tip", text: "Tap me!")
 }
