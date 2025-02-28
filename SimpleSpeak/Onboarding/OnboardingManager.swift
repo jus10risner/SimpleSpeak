19f4d165bab54ff830d7051cc4c451654f5eb08b
Justin <justinrisner@gmail.com>
2025-02-28 16:38:42 -0500

    [Feature] Switched @Published values used during onboarding setup over to @AppStorage, for more permanent saving

--- SimpleSpeak/Onboarding/OnboardingManager.swift
+++ SimpleSpeak/Onboarding/OnboardingManager.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 class OnboardingManager: ObservableObject {
-    @Published var currentStep: OnboardingStep = .welcome
+    @AppStorage("currentStep") var currentStep: OnboardingStep = .welcome
+    @AppStorage("isComplete") var isComplete: Bool = false
     
-//    @AppStorage("currentStep") var currentStep: OnboardingStep = .welcome
-//    @AppStorage("isComplete") var isComplete: Bool = true
     @AppStorage("isShowingWelcomeView") var isShowingWelcomeView: Bool = true
     @AppStorage("isShowingMultiButtonTip") var isShowingMultiButtonTip: Bool = false
-//    @AppStorage("isShowingManageCategoryTip") var isShowingManageCategoryTip: Bool = false
+    @AppStorage("isShowingManageCategoryTip") var isShowingManageCategoryTip: Bool = false
     
-    @Published var isComplete: Bool = false
+//    @Published var currentStep: OnboardingStep = .welcome
+//    @Published var isComplete: Bool = false
 //    @Published var isShowingWelcomeView: Bool = true
 //    @Published var isShowingMultiButtonTip: Bool = false
-    @Published var isShowingManageCategoryTip: Bool = false
+//    @Published var isShowingManageCategoryTip: Bool = false
     
     func showWelcome() {
         if isComplete == false && currentStep == .welcome {
